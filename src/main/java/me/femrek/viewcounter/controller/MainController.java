8979ea0116dd63082ad90f9f9be69c9d166cd5a8
Faruk Emre EKŞİOĞLU <faruk.eksioglu@hotmail.com>
2025-07-09 18:56:41 +0300

    feat: add view counter to index page.
    * Refactor ui.

--- src/main/java/me/femrek/viewcounter/controller/MainController.java
+++ src/main/java/me/femrek/viewcounter/controller/MainController.java
@@ -6,6 +6,7 @@ import me.femrek.viewcounter.dto.GithubUserDTO;
 import me.femrek.viewcounter.dto.SubscriptionDTO;
 import me.femrek.viewcounter.security.CustomOAuth2User;
 import me.femrek.viewcounter.service.SubscriptionService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,6 +18,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class MainController {
     private final SubscriptionService subscriptionService;
 
+    @Value(value = "${requestTrackerUrl:#{null}}")
+    private String requestTrackerUrl;
+
     @GetMapping("/")
     public String index(@AuthenticationPrincipal CustomOAuth2User user, Model model) {
         if (user != null && user.getGithubUser() != null) {
@@ -24,6 +28,10 @@ public class MainController {
             subscriptionService.mountSubscriptions(user.getGithubUser());
             model.addAttribute("subscriptions",
                     SubscriptionDTO.from(user.getGithubUser().getSubscriptions()));
+        }
+
+        if (requestTrackerUrl != null) {
+            model.addAttribute("requestTrackerUrl", requestTrackerUrl);
         }
 
         return "index";
